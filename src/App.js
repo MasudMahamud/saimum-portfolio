6cd69c3d8fa4f05a8d3ef203c4be992c693032d2
MasudMahamud <mahamud.al.masud@gmail.com>
2022-05-30 17:36:27 +0600

    preloader

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import './App.css';
-import Home from './Components/Home/Home';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Home from "./Components/Home/Home";
+import HashLoader from "react-spinners/HashLoader";
 
 function App() {
+  let [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
   return (
     <>
-      <Home/>     
+      {loading ? (
+        <div className="load">
+          <HashLoader color={"#fb5200"} loading={loading} size={70} />
+        </div>
+      ) : (
+        <Home />
+      )}
     </>
   );
 }
